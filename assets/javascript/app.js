4337f341a82876a6cc30b1a9a1b7eccff5cb018c
EpicureanHeron <joethor1989@gmail.com>
2018-06-07 12:56:09 -0500

    timer now counts down on screen

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,53 +2,60 @@
 
 
 // prevents the clock from being sped up unnecessarily
-
-var counter = 0;
-
+//updated each time a quesiton is created
 var questionNumber = 0;
+//timer
+var timerValue = 30
+//variablse to be used after the answer is submitted
+var correctGuesses;
 
-newObject = "";
+var incorrectGuesses;
 
+//starts the timer which calls the count function 
 function startTimer() {
-
-    //  TODO: Use setInterval to start the count here and set the clock to running.
-    
-    setInterval(count, 1000);
-    
-  
+	//probably should set this to a variable so that I can refer back to it and reset it ? 
+    setInterval(count, 1000); 
   }
   
 function count() {
-	
-	counter = counter + 1
-	
-	console.log("hi! " + counter)
+	//counts down and updates the screen
+	timerValue = timerValue-1
+	$(".timer").html(timerValue)
 }
 
 //Creates and returns objects that are questions
-function createQuestionObj(objectName, questionString, guessArr, correctGuessIndex) {
-	
-	
-	
+function createQuestionObj(questionString, guessArr, correctGuessIndex) {	
 	newObject = {
-		 name: objectName,
 		 //questionNumber is a global variable
 		 number: questionNumber,
+		 //Main question 
 		 mainQuestion: questionString,
+		 //options for the quesiton
 		 options: guessArr,
+		 //sets the value for correct guess based of a passed in value, correctGuessIndex which refers back to the guessArr
 		 correctGuess: guessArr[correctGuessIndex]
 	}
+	//updates the question number
 	questionNumber ++ 
+	//returns the new object
 	return newObject
-
 }
-  
-//startTimer()
+ //takes a question (which needs to be an object) as a parameter and writes it to the page
+function updatePage(question) {
+	//displays question
+	$(".question").html(question.mainQuestion)
+	//displays all options
+	for (i = 0; i < question.options.length; i ++) {		
+		var classToUpdate = ".option" + i	
+		var optionNumber = i + 1
+		$(classToUpdate).html(optionNumber + ": " + question.options[i])	
+	}
+} 
+//basically this is how you can create a quesiton object
+var newQuestion = createQuestionObj("How many poots does a pooter poot?", ["hey hey", "My My"], 0)
 
+var newQuestion2 = createQuestionObj("I Don't know!", ["this is an array", "Baddass", "monster"], 1)
 
+startTimer()
 
-var newQuestion = createQuestionObj("Testing", "How many poots does a pooter poot?", ["1: hey hey", "2: My My"], 0)
-
-var newQuestion2 = createQuestionObj("basterman!", "I Don't know!", ["this is an array", "Baddass", "monster"], 1)
-
-console.log(newQuestion2.correctGuess)
+updatePage(newQuestion2)
